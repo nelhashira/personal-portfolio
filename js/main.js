e575bd563580ca136ea72ff8265c71dcdb8308e0
nelhashira <nelpopuli@gmail.com>
2020-07-25 17:07:23 +0800

    Making the image modal with slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,65 @@
 const selectElement = (s) => document.querySelector(s);
+const selectElementAll = (s) => document.querySelectorAll(s);
+
+const toggleOpen = selectElement("#toggle-open");
+const toggleCLose = selectElement("#toggle-close");
+const navBar = selectElement("#nav-bar-open");
 
 // Toggle open on click
-selectElement("#toggle-open").addEventListener("click", () => {
-   selectElement("#nav-bar-open").classList.add("toggle");
+toggleOpen.addEventListener("click", () => {
+   navBar.classList.add("toggle");
 });
 
 // Toggle close on click
-selectElement("#toggle-close").addEventListener("click", () => {
-   selectElement("#nav-bar-open").classList.remove("toggle");
+toggleCLose.addEventListener("click", () => {
+   navBar.classList.remove("toggle");
 });
+
+const toggleModal = selectElementAll(".open-project-modal");
+const myModal = selectElement("#project-modal");
+
+// Open Project Modal
+toggleModal.forEach((btn) => {
+   btn.addEventListener("click", () => {
+      myModal.style.display = "block";
+   });
+});
+
+// CLose Modal
+myModal.addEventListener("click", (e) => {
+   // myModal.style.display = "none";
+   if (e.target.classList.contains("project-modal-bg")) {
+      myModal.style.display = "none";
+   }
+});
+
+// SLideshow
+const slides = selectElementAll(".slide");
+const prev = selectElement("#prev");
+const next = selectElement("#next");
+
+var slideIndex = 1;
+
+prev.addEventListener("click", () => {
+   slideShow(--slideIndex);
+});
+
+next.addEventListener("click", () => {
+   slideShow(++slideIndex);
+});
+
+const slideShow = (index) => {
+   if (index > slides.length) {
+      slideIndex = 1;
+   }
+
+   if (index < 1) {
+      slideIndex = slides.length;
+   }
+
+   for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+   }
+   slides[slideIndex - 1].style.display = "block";
+};
+slideShow(slideIndex);
