e58638fd75b82e712f1557c8fda38978236bb1f6
nelhashira <nelpopuli@gmail.com>
2020-07-25 10:48:36 +0800

    Chnage the projects section

--- js/main.js
+++ js/main.js
@@ -9,13 +9,3 @@ selectElement("#toggle-open").addEventListener("click", () => {
 selectElement("#toggle-close").addEventListener("click", () => {
    selectElement("#nav-bar-open").classList.remove("toggle");
 });
-
-// Mouse over
-selectElement("#project-item-container").addEventListener("mouseover", () => {
-   selectElement("#project-hover-modal").classList.add("active-hover");
-});
-
-// Mouse out
-selectElement("#project-item-container").addEventListener("mouseout", () => {
-   selectElement("#project-hover-modal").classList.remove("active-hover");
-});
